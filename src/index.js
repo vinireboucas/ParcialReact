8ab6a34aaf3a00da27258b11958ef6640f883f8b
vinireboucas <vini.reboucas2@gmail.com>
2022-04-18 20:23:13 -0300

    fds

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import reactDom from "react-dom";
+import "./style.css"
+import batmanBG from "../src/img/bg.png"
+import profilePic from "../src/img/profilePic.png"
+let site = (
+  <>
+    
+    <header>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+        <div>
+            <p>Descuentos 20%</p>
+        </div>
+
+        <div className="heroes">
+            <h2>Heroes</h2>
+        </div>
+
+    </header>
+
+    <main>
+    <div className="card"> 
+            <img src={batmanBG} alt="bkg batman" />
+            
+            <div className="card-content">
+                <img src={profilePic} alt="profilePicBatman" />
+                <h3>BATMAN</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ullam deserunt numquam, ex consectetur beatae molestiae magnam pariatur dolore soluta?</p>
+                <button> Ver más</button>
+            </div>
+            
+        </div>
+        <div className="card"> 
+            <img src={batmanBG} alt="bkg batman" />
+            
+            <div className="card-content">
+                <img src={profilePic} alt="profilePicBatman" />
+                <h3>BATMAN</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ullam deserunt numquam, ex consectetur beatae molestiae magnam pariatur dolore soluta?</p>
+                <button> Ver más</button>
+            </div>
+            
+        </div>
+        <div className="card"> 
+            <img src={batmanBG} alt="bkg batman" />
+            
+            <div className="card-content">
+                <img src={profilePic} alt="profilePicBatman" />
+                <h3>BATMAN</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ullam deserunt numquam, ex consectetur beatae molestiae magnam pariatur dolore soluta?</p>
+                <button> Ver más</button>
+            </div>
+            
+        </div>
+
+       
+    </main>
+
+    <footer>
+            <p>Todos los derechos reservados a FRONT END 1</p>
+    </footer>
+
+  </>
 );
+reactDom.render(site, document.getElementById("root"));
